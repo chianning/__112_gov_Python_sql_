fe40daddfdd9f1ac3f36642cacd893b69729ccf7
范家寧 <161096160+chianning@users.noreply.github.com>
2024-03-09 14:55:18 +0800

    Update lesson5_7.sql

--- lesson5_7.sql
+++ lesson5_7.sql
@@ -8,5 +8,16 @@ FROM gate_count LEFT JOIN stations ON 站點編號=編號
 	
 SELECT DISTINCT 名稱
 FROM gate_count LEFT JOIN stations ON 站點編號=編號
-WHERE 地址 LIKE '高雄市%';
+WHERE 地址 LIKE '基隆市%';
 
+/*取出基隆火車站2022年3月1日資料*/
+SELECT 日期,名稱
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+WHERE 日期 ='2020-03-01%' AND 名稱='基隆';
+
+/*取出基隆火車站2022年3月份資料,時間由小到大*/
+
+SELECT 日期,進站人數,出站人數,名稱,地名,地址,youbike
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+WHERE 名稱='基隆' AND (日期 BETWEEN '2022-03-01' AND '2022-03-31')
+ORDER BY 日期 ASC;
