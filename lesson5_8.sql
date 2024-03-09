bfcbbc5a7f4563c87203ade03f59f873697f10d3
范家寧 <161096160+chianning@users.noreply.github.com>
2024-03-09 15:51:44 +0800

    Create lesson5_8.sql

---
+++ lesson5_8.sql
@@ -0,0 +1,23 @@
+/*取出payment的所有客戶的coustomer_id(不重複)*/
+SELECT customer_id, COUNT(*) AS 筆數
+FROM payment
+GROUP BY customer_id
+
+/*取出payment的所有客戶的應收帳總和,SUM()*/
+
+SELECT customer_id, SUM(amount) AS 總和
+FROM payment
+GROUP BY customer_id
+ORDER BY 總和 DESC;
+
+SELECT first_name || ' ' || last_name AS full_name,
+	 SUM(amount) AS 總和
+FROM payment LEFT JOIN customer ON payment.customer_id=customer.customer_id
+GROUP BY full_name
+ORDER BY 總和 DESC;
+
+SELECT first_name || ' ' || last_name AS full_name,
+	 SUM(amount) AS 總和
+FROM payment LEFT JOIN customer USING (customer_id)
+GROUP BY full_name
+ORDER BY 總和 DESC;
