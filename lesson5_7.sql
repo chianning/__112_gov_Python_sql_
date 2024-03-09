543b0173966b4212becf6a4e5f1bb7d4316e79f9
范家寧 <161096160+chianning@users.noreply.github.com>
2024-03-09 14:40:02 +0800

    Create lesson5_7.sql

---
+++ lesson5_7.sql
@@ -0,0 +1,12 @@
+SELECT *
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+
+SELECT 日期,進站人數,出站人數,名稱,地名,地址,youbike
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+
+/*取出基隆市有哪些火車站*/
+	
+SELECT DISTINCT 名稱
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+WHERE 地址 LIKE '高雄市%';
+
