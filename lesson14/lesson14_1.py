40bb7220b9951b0261bb014be7cb2854029d6262
范家寧 <161096160+chianning@users.noreply.github.com>
2024-04-13 06:17:24 +0000

    modify

---
+++ lesson14/lesson14_1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write('Hello, *whorld!* :sunglasses:')
+st.markdown('## 這是標題!')
+st.title("我是title!")
+st.header("我是header")
+st.subheader("我是sub header!")
+
+col1,col2,col3 = st.columns(3)
