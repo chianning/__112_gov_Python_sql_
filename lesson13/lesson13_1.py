19f1d041fe8c356857105f49125393481fe11bcc
范家寧 <161096160+chianning@users.noreply.github.com>
2024-04-13 03:48:25 +0000

    modify

---
+++ lesson13/lesson13_1.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import psycopg2
+import os
+conn = psycopg2.connect(os.environ['POSTGRE_PASSWORD'])
+
+with psycopg2.connect(os.environ['POSTGRE_PASSWORD']) as conn:
+    with conn.cursor() as cursor:
+        sql = '''
+            SELECT 聯絡人.聯絡人id,客戶名稱,聯絡人姓名,電話,郵件
+            FROM 聯絡人 LEFT JOIN 客戶 ON 聯絡人.客戶id =客戶.客戶_id
+        '''
+        cursor.execute(sql)
+        datas:list  = cursor.fetchmany(10)
+       
+for item in datas:
+    print(item)
