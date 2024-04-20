1ffd37cee63fb51e03d4d3945f5e69fdeb92462f
范家寧 <161096160+chianning@users.noreply.github.com>
2024-04-20 03:55:10 +0000

    modify

--- lesson15/lesson15_3.py
+++ lesson15/lesson15_3.py
@@ -1,16 +1,3 @@
-import streamlit as st
-
-#建立預設的session_state的key
-if 'a1' not in st.session_state:
-    st.session_state['a1'] = 100
-
-if 'a2' not in st.session_state:
-    st.session_state['a2'] = 200
-
-if 'a3' not in st.session_state:
-    st.session_state['a3'] = 300
-
-st.write(st.session_state)
 
 import streamlit as st
 
