d37c1d260feac339b9a701ea06e7a712964e9bdc
范家寧 <161096160+chianning@users.noreply.github.com>
2024-04-20 03:52:18 +0000

    modify

--- lesson15/lesson15_3.py
+++ lesson15/lesson15_3.py
@@ -11,3 +11,14 @@ if 'a3' not in st.session_state:
     st.session_state['a3'] = 300
 
 st.write(st.session_state)
+
+import streamlit as st
+
+if 'count' not in st.session_state:
+    st.session_state['count'] = 0
+
+increment = st.button("增加計數器的值",key="mybutton")
+if increment:
+   st.session_state['count'] += 1
+
+st.write("計數器:",st.session_state.count)
