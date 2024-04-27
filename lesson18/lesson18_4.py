1b7b6830c2239e798e55802d081ab29194e944e4
范家寧 <161096160+chianning@users.noreply.github.com>
2024-04-27 08:05:30 +0000

    modify

--- lesson18/lesson18_4.py
+++ lesson18/lesson18_4.py
@@ -9,21 +9,21 @@ load_dotenv()
 def get_sarea()->tuple:
     conn = psycopg2.connect(os.environ['POSTGRE_PASSWORD1'])
     with conn:
-        with conn.cursor() as cursor: 
-         #取出最新日期各站點資料
-         sql = '''
-              SELECT 行政區
-              FROM 站點資訊
-              GROUP BY 行政區
-         '''
-         cursor.execute(sql)
-         allArea:tuple = cursor.fetchall()
-         return allArea
+        with conn.cursor() as cursor:
+            #取出最新日期各站點資料
+            sql = '''
+                SELECT 行政區
+                FROM 站點資訊
+                GROUP BY 行政區
+            '''
+            cursor.execute(sql)
+            allArea:tuple = cursor.fetchall()
+            return allArea
     
     conn.close()
 
 col1,col2 = st.columns([1,2])
-data = [tuple[0]  for tuple1 in get_sarea()]
+data = [tuple1[0]  for tuple1 in get_sarea()]
 #print(data)
 
 st.radio('選擇行政區:',data,key ='sarea',horizontal=True)
